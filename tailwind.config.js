586b81b5b544f8c7634e0eda022073c4bff68673
william <william.odiomonafe@gmail.com>
2021-10-20 13:34:05 +0100

    Created waitlist page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'waitlist': "url('/images/waitlist.png')",
+        'gadgets': "url('/images/gadgets.png')"
+      }
+    },
   },
   variants: {
     extend: {},
