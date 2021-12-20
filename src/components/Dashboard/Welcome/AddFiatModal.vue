0fe5ad877e8493bc25276aa3cb43bf0d1480ee67
william <william.odiomonafe@gmail.com>
2021-12-20 01:41:53 +0100

    Added fiat selector

--- src/components/Dashboard/Welcome/AddFiatModal.vue
+++ src/components/Dashboard/Welcome/AddFiatModal.vue
@@ -1,37 +1,59 @@
 <template>
-  <div class="p-6 w-full md:w-1/2 mt-6 mx-4 md:mx-auto bg-white rounded-xl text-center shadow-lg">
-    <h3 class="font-bold text-lg">Set up your local currency</h3>
+  <Dialog
+      :open="modalIsOpen"
+      @close="setIsOpen"
+      class="fixed inset-0 z-10 overflow-y-auto"
+  >
+    <div class="flex items-center justify-center min-h-screen">
+      <DialogOverlay class="fixed inset-0 bg-black opacity-75" />
 
+      <div class="relative md:w-4/12 mx-auto bg-white rounded-xl">
+        <h3 class="font-medium text-md my-3 mx-8">&#128176; Set up your local currency</h3>
 
-    <div class="mt-8">
-      <h6 class="text-sm text-left">Select Local Currency</h6>
-      <Multiselect :options="fiats"
-                   v-model="selectedFiat"
-                   :searchable="true"
-                   :required="true"
-      >
-      </Multiselect>
+        <hr class="my-2">
+
+        <div class="m-8">
+          <h6 class="text-sm text-left">Select Local Currency</h6>
+          <Multiselect :options="fiats"
+                       v-model="selectedFiat"
+                       :searchable="true"
+                       :required="true"
+          >
+          </Multiselect>
+
+          <div class="mt-12 flow-root">
+            <button class="float-left rounded border border-blue-600 py-2 px-6 w-24 text-blue-600" @click="$parent.gotoSetupStep('selectAssetLogger')">Skip</button>
+            <button class="float-right rounded bg-blue-600 py-2 px-6 w-24 text-white" @click="saveSelectedFiat">Save</button>
+          </div>
+        </div>
+
+      </div>
     </div>
-
-    <div class="mt-12 flow-root">
-      <button class="float-left rounded border-2 border-blue-600 py-2 px-6 w-24 text-blue-600" @click="$parent.gotoSetupStep('selectAssetLogger')">Skip</button>
-      <button class="float-right rounded bg-blue-600 py-2 px-6 w-24 text-white" @click="saveSelectedFiat">Save</button>
-    </div>
-  </div>
+  </Dialog>
 </template>
 
 <script>
 import Axios from "../../../../config/axios";
 import Multiselect from '@vueform/multiselect';
 import '@vueform/multiselect/themes/default.css';
+import {
+  Dialog,
+  DialogOverlay,
+} from "@headlessui/vue";
 
 export default {
   name: "AddFiatModal",
-  components: { Multiselect },
+  props: {
+    isOpen: {
+      type: Boolean
+    }
+  },
+  components: { Multiselect, Dialog, DialogOverlay },
   data() {
     return {
       fiats: [],
-      selectedFiat: null
+      selectedFiat: null,
+      modalIsOpen: this.isOpen,
     }
   },
   async created() {
@@ -55,12 +77,16 @@ export default {
         this.showSuccessToast("Fiat/local currency has been set.");
 
         this.$store.state.user.fiat_id = this.selectedFiat;
-        this.gotoSetupStep('selectAssetLogger');
+        this.$emit('savedFiatCurrency');
       } catch (e) {
         console.error(e);
       } finally {
         this.hideLoader();
       }
+    },
+
+    setIsOpen(value) {
+      this.modalIsOpen = value;
     }
   }
 }
