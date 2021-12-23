1c3b077ad8a5fbe1adb4a98bbc010298e872bef5
william <william.odiomonafe@gmail.com>
2021-12-23 08:39:34 +0100

    Fix mobile responsiveness of homepage

--- src/components/Dashboard/Welcome/WelcomeModal.vue
+++ src/components/Dashboard/Welcome/WelcomeModal.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="w-full">
-    <div v-if="!isLogSelectorOpen" class="w-full md:w-full mt-6 mx-4 py-20 text-center md:mx-auto bg-white rounded-xl text-left shadow">
-      <h1 class="text-gray-700 font-bold text-4xl">Welcome to Assetlog</h1>
+    <div v-if="!isLogSelectorOpen" class="w-full mt-6 px-5 md:py-20 py-10 text-center md:mx-auto bg-white rounded-xl text-left shadow">
+      <h1 class="text-gray-700 font-bold md:text-4xl text-2xl">Welcome to Assetlog</h1>
 
-      <h3 class="text-2xl text-gray-400 my-6">Great to have you on board! Feel free to explore, or get a head start below.</h3>
+      <h3 class="md:text-2xl text-xl text-gray-400 my-6">Great to have you on board! Feel free to explore, or get a head start below.</h3>
 
-      <div class="grid grid-cols-2 m-20 gap-x-10">
+      <div class="grid md:grid-cols-2 md:m-20 gap-x-10 gap-y-10">
         <div style="cursor: pointer" class="border-2 border-gray-300 hover:border-blue-500 rounded-3xl p-2 text-center">
           <img src="/images/options.png" class="m-auto" />
 
           <h3 class="mt-5 font-bold text-2xl">Take a mini tour</h3>
-          <p class="text-xl text-gray-400 mx-10 my-5">Watch a walkthrough process of how assetlog works and how to use it.</p>
+          <p class="text-xl text-gray-400 mx-10 my-5 md:block hidden">Watch a walkthrough process of how assetlog works and how to use it.</p>
         </div>
 
         <div @click="openFiatSelector()"
@@ -19,7 +19,7 @@
           <img src="/images/layer.png" class="m-auto" />
 
           <h3 class="mt-5 font-bold text-2xl">Log your first asset</h3>
-          <p class="text-xl text-gray-400 mx-10 my-5">Waste no time getting started. Proceed to log your first asset!</p>
+          <p class="text-xl text-gray-400 mx-10 my-5 md:block hidden">Waste no time getting started. Proceed to log your first asset!</p>
         </div>
       </div>
     </div>
