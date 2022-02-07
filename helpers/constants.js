c5feed524d708bd812d74a9049f75fd103354147
william <william.odiomonafe@gmail.com>
2022-02-07 14:47:33 +0100

    Fix Jakes email

--- helpers/constants.js
+++ helpers/constants.js
@@ -2,6 +2,6 @@ export const DEV_WHITELIST = [
     "william.odiomonafe@gmail.com",
     "kamal.misc@gmail.com",
     "temmyjay001@gmail.com",
-    "jaketuriacada@gmail.co",
+    "jaketuriacada@gmail.com",
     "morenikeade@gmail.com"
 ];
