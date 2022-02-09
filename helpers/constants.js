3d7e5fa052a87629c68c1e118dce2211effe66cf
william <william.odiomonafe@gmail.com>
2022-02-09 22:54:18 +0100

    Temporarily comment out Sign in with Google link

--- helpers/constants.js
+++ helpers/constants.js
@@ -9,4 +9,6 @@ export const DEV_WHITELIST = [
     "georgemerit38@gmail.com",
     "adamrick1992@gmail.com",
     "ewahifeanyi780@gmail.com",
+    "zamat4ever@gmail.com",
+    "oshovic2@gmail.com"
 ];
