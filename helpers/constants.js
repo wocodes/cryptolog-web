f973cbc80815ed27c9fb2ba430fd773e480d2b2b
william <william.odiomonafe@gmail.com>
2022-02-09 11:43:01 +0100

    Added more testers

--- helpers/constants.js
+++ helpers/constants.js
@@ -2,6 +2,10 @@ export const DEV_WHITELIST = [
     "william.odiomonafe@gmail.com",
     "kamal.misc@gmail.com",
     "temmyjay001@gmail.com",
+    "morenikeade@gmail.com",
+
     "jaketuriacada@gmail.com",
-    "morenikeade@gmail.com"
+    "georgemerit38@gmail.com",
+    "adamrick1992@gmail.com",
+    "ewahifeanyi780@gmail.com",
 ];
